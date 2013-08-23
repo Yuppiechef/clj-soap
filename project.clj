9d6e212aef75371fdb02b5118a08f656bb69ef52
Jeremy Axmacher <jeremy@obsoleter.com>
2013-08-23 13:00:59 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.obsoleter/clj-soap "0.2.2-SNAPSHOT"
+(defproject org.clojars.obsoleter/clj-soap "0.2.3-SNAPSHOT"
   :description "SOAP Client and Server using Apache Axis2."
   :url "https://github.com/seancorfield/clj-soap"
   :license {:name "Eclipse Public License"
