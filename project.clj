e486357cbd7247d75553fcec21bd4f16db0cb544
Jeremy A <jeremy@obsoleter.com>
2013-08-23 02:15:44 -0400

    Add clojure.data.json dependency
    Previous commit is broken without this dependency.

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/core.incubator "0.1.2"]
                  [org.apache.axis2/axis2-adb "1.6.2"]
                  [org.apache.axis2/axis2-transport-http "1.6.2"]
-                 [org.apache.axis2/axis2-transport-local "1.6.2"]]
+                 [org.apache.axis2/axis2-transport-local "1.6.2"]
+                 [org.clojure/data.json "0.2.2"]]
   :source-paths ["src" "test"]
   :aot [clj-soap.test.core])
 
